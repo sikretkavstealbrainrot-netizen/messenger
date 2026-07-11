fdd0d4077b7253116b23ac4a2b5d54b9b1ab6d1d
sikretkavstealbrainrot-netizen <sikretkavstealbrainrot@gmail.com>
2026-07-11 14:28:00 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,10 @@
 // (Project settings -> General -> Your apps -> SDK setup and configuration)
 // ============================================================
 export const firebaseConfig = {
-  apiKey: "ВАШ_API_KEY",
-  authDomain: "ВАШ_ПРОЕКТ.firebaseapp.com",
-  projectId: "ВАШ_ПРОЕКТ",
-  storageBucket: "ВАШ_ПРОЕКТ.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx"
-};
+  apiKey: "AIzaSyDc0J45eZoWqw34IqRL_tfd0_J1585qafo",
+  authDomain: "aurora-messenge.firebaseapp.com",
+  projectId: "aurora-messenge",
+  storageBucket: "aurora-messenge.firebasestorage.app",
+  messagingSenderId: "239989409925",
+  appId: "1:239989409925:web:e76b923d55358fe54c41ec
+    };
